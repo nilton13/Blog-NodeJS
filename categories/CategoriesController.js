2c2b0c1062b586c3280640f4360d92340c835b24
nilton13 <jnvlfilho@hotmail.com>
2021-10-02 12:49:32 -0300

    Criando formulario de criaçao de catgorias e rota para a criaçao de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
-router.get("/categories", (req,res) =>{
-    res.send("ROTA DE CATEGORIAS")
+router.get("/admin/categories/new",(req,res) =>{
+    res.render("admin/categories/new");
 });
 
-router.get("/admin/categories/new", (req,res) => {
-    res.send("ROTA PARA CRIAR CATEGORIA!")
-})
+router.post("/categories/save", (req,res) =>{
+    var title = req.body.title;
+    if(title != undefined){
+
+        Category.create({
+            title: title,
+            slug:  slugify(title)
+        }).then(() =>{
+            res.redirect("/"); 
+        })
+
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+});
 
 module.exports = router;
