372cbcb081f7179892b95973a70b4eba4b71173b
nilton13 <jnvlfilho@hotmail.com>
2021-10-02 13:33:22 -0300

    Criando açao de deletar categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -30,4 +30,27 @@ router.get("/admin/categories", (req, res) => {
     })
 });
 
+router.post("/categories/delete", (req,res) =>{
+    var id = req.body.id;
+
+    if(id != undefined){
+        
+        if(!isNaN(id)){
+
+            Category.destroy({
+                where:{
+                    id:id
+                }
+            }).then(() =>{
+                res.redirect("/admin/categories");
+            })
+
+        }else{ // NÃO FOR UM NÚMERO
+            res.redirect("/admin/categories");
+        }
+    }else{ // NULL
+        res.redirect("/admin/categories"); 
+    }
+})
+
 module.exports = router;
